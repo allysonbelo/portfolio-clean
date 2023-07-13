d272ccdfa0e0fdc7e9b8c216b5c5afbe530242df
allysonbelo <allysonbelo@gmail.com>
2023-07-13 19:58:38 -0300

    Add: Adicionado a seção de skill e projetos

--- front-page.php
+++ front-page.php
@@ -4,10 +4,9 @@
     <main class="main__frontpage">
         <section><?php get_template_part('template-parts/content', 'banner'); ?></section>
         <section id="about" class="about__section"><?php get_template_part('template-parts/content', 'about'); ?></section>
-        <section>Projects</section>
+        <section id="projects" class="about__projects"><?php get_template_part('/template-parts/content', 'projects'); ?></section>
         <section>Contact</section>
     </main>
-
 </div>
 
 <?php get_footer() ?>
---
+++ template-parts/content-projects.php
@@ -0,0 +1,46 @@
+<h2 class="projects__title">My Projects</h2>
+
+<div class="projects__cards">
+    <?php
+
+    $show_posts_number = get_field('number_of_posts');
+
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => $show_posts_number,
+    );
+    $customLoop = new WP_Query($args);
+
+    if ($customLoop->have_posts()) :
+        while ($customLoop->have_posts()) : $customLoop->the_post(); ?>
+
+            <article class="projects__card--article">
+                <?php the_post_thumbnail("medium") ?>
+                <div class="projects__card--content">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h3>
+                </div>
+            </article>
+
+        <?php endwhile;
+        wp_reset_postdata();
+    else : ?>
+        <p>Nothing yet to be displayd!</p>
+    <?php endif;
+    ?>
+
+</div>
+
+<?php
+$args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+);
+
+$query = new WP_Query($args);
+$total_posts = $query->found_posts;
+
+if ($total_posts > $show_posts_number) {
+    echo '<a class="" href="https://preview.colorlib.com/#ronin" target="_blank">Show all projects</a>';
+}
+
+?>

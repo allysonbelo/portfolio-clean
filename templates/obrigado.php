b0d8399b44e1b4cf80239b8936686f2a92626fb9
allysonbelo <allysonbelo@gmail.com>
2023-08-01 15:08:29 -0300

    Add: Contact section done

---
+++ templates/obrigado.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Template Name: Obrigado
+ * Template Post Type: page
+ */
+
+get_header();
+
+?>
+
+<div class="wrapper" style="padding-top: 120px; padding-bottom: 120px;">
+    <h2><?php echo get_the_title(); ?></h2>
+
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
+    <hr>
+    <div id="countdown" style="font-size: 24px; text-align: center;"></div>
+</div>
+
+<!-- Adicionar o código JavaScript para a contagem decrescente -->
+<script>
+    // Função para exibir a contagem decrescente
+    function countdown() {
+        let timeLeft = 10; // Tempo inicial em segundos
+
+        const countdownElement = document.getElementById('countdown');
+
+        const interval = setInterval(function () {
+            countdownElement.innerHTML = timeLeft + ' segundos';
+
+            if (timeLeft <= 1) {
+                clearInterval(interval);
+                countdownElement.innerHTML = 'Redirecionando...';
+                // Redirecionar para a página inicial após 10 segundos
+                window.location.href = '<?php echo esc_url(home_url()); ?>';
+            }
+
+            timeLeft--;
+        }, 1000); // 1000 milissegundos = 1 segundo
+    }
+
+    // Iniciar a contagem quando a página for carregada
+    window.onload = function () {
+        countdown();
+    };
+</script>
+
+<?php get_footer(); ?>

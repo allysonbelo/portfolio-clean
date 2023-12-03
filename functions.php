14172f3d25c98339bebea45935aab63ce392caaf
allysonbelo <allysonbelo@gmail.com>
2023-12-03 08:40:53 -0300

    Fix: código do microsoft clarity comentado

--- functions.php
+++ functions.php
@@ -100,25 +100,25 @@ function add_google_analytics()
 
 add_action('wp_head', 'add_google_analytics');
 
-function add_microsoft_clarity()
-{
-?>
-    <script defer type="text/javascript">
-        (function(c, l, a, r, i, t, y) {
-            c[a] = c[a] || function() {
-                (c[a].q = c[a].q || []).push(arguments)
-            };
-            t = l.createElement(r);
-            t.async = 1;
-            t.src = "https://www.clarity.ms/tag/" + i;
-            y = l.getElementsByTagName(r)[0];
-            y.parentNode.insertBefore(t, y);
-        })(window, document, "clarity", "script", "jtihy1q48l");
-    </script>
-<?php
-}
+// function add_microsoft_clarity()
+// {
+// ?>
+//     <script defer type="text/javascript">
+//         (function(c, l, a, r, i, t, y) {
+//             c[a] = c[a] || function() {
+//                 (c[a].q = c[a].q || []).push(arguments)
+//             };
+//             t = l.createElement(r);
+//             t.async = 1;
+//             t.src = "https://www.clarity.ms/tag/" + i;
+//             y = l.getElementsByTagName(r)[0];
+//             y.parentNode.insertBefore(t, y);
+//         })(window, document, "clarity", "script", "jtihy1q48l");
+//     </script>
+// <?php
+// }
 
-add_action('wp_head', 'add_microsoft_clarity');
+// add_action('wp_head', 'add_microsoft_clarity');
 
 function add_font_awesome()
 {
